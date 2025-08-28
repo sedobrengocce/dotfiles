4069081150b11f50efbdccedf01d070c2c70cdfc
Giuseppe Tufo <pepj2000@gmail.com>
2025-08-28 23:03:03 +0200

    fixing flutter lint and formatting

--- nvim/.config/nvim/lua/pepj/plugins/lsp/conform.lua
+++ nvim/.config/nvim/lua/pepj/plugins/lsp/conform.lua
@@ -1,32 +1,33 @@
 return {
-    'stevearc/conform.nvim',
-    opts = {
-        formatters_by_ft = {
-            lua = { 'stylua' },
-            javascript = { 'prettier' },
-            typescript = { 'prettier' },
-            html = { 'prettier' },
-            css = { 'prettier' },
-            json = { 'prettier' },
-            yaml = { 'prettier' },
-            markdown = { 'prettier' },
-            sh = { 'shfmt' },
-            go = { 'gofmt' },
-            cpp = { 'clang_format' },
-            c = { 'clang_format' },
-            bash = { 'shfmt' },
-            dart = { 'dart_format' },
-        },
-        format_on_save = false,
-    },
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function(_, opts)
-        require('conform').setup(opts)
-        vim.api.nvim_create_autocmd('BufWritePre', {
-            pattern = '*',
-            callback = function()
-                require('conform').format({ async = false })
-            end,
-        })
-    end,
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			html = { "prettier" },
+			css = { "prettier" },
+			json = { "prettier" },
+			yaml = { "prettier" },
+			markdown = { "prettier" },
+			sh = { "shfmt" },
+			go = { "gofmt" },
+			cpp = { "clang_format" },
+			c = { "clang_format" },
+			bash = { "shfmt" },
+			dart = { "dart_format" },
+		},
+		format_on_save = false,
+	},
+	event = { "BufReadPre", "BufNewFile" },
+	config = function(_, opts)
+		local conform = require("conform")
+		conform.setup(opts)
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function()
+				conform.format({ async = false })
+			end,
+		})
+	end,
 }
--- nvim/.config/nvim/lua/pepj/plugins/lsp/nvim-lint.lua
+++ nvim/.config/nvim/lua/pepj/plugins/lsp/nvim-lint.lua
@@ -1,28 +1,53 @@
 return {
-    "mfussenegger/nvim-lint",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        local lint = require("lint")
-        lint.linters_by_ft = {
-            lua = { "luacheck" },
-            javascript = { "eslint" },
-            typescript = { "eslint" },
-            html = { "eslint" },
-            css = { "stylelint" },
-            json = { "eslint" },
-            yaml = { "yamllint" },
-            markdown = { "markdownlint" },
-            sh = { "shellcheck" },
-            go = { "golangcilint" },
-            cpp = { "clang_check" },
-            c = { "clang_check" },
-            bash = { "bach" },
-            dart = { "dartanalyzer" },
-        }
-        vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-            callback = function()
-                lint.try_lint()
-            end,
-        })
-    end,
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local lint = require("lint")
+
+		lint.linters.flutter_analyze = {
+			cmd = "flutter",
+			args = { "analyze", "--no-fatal-infos", "--no-fatal-warnings", "$FILENAME" },
+			stream = "stdout",
+			ignore_exitcode = true,
+			parser = function(output, _)
+				local diagnostics = {}
+				for line in output:gmatch("[^\r\n]+") do
+					local file, line_num, col_num, message = line:match("^(.-):(%d+):(%d+): (.+)$")
+					if file and line_num and col_num and message then
+						table.insert(diagnostics, {
+							filename = file,
+							lnum = tonumber(line_num) - 1,
+							col = tonumber(col_num),
+							message = message,
+							severity = vim.lsp.protocol.DiagnosticSeverity.Warning,
+						})
+					end
+				end
+				return diagnostics
+			end,
+		}
+
+		lint.linters_by_ft = {
+			lua = { "luacheck" },
+			javascript = { "eslint" },
+			typescript = { "eslint" },
+			html = { "eslint" },
+			css = { "stylelint" },
+			json = { "eslint" },
+			yaml = { "yamllint" },
+			markdown = { "markdownlint" },
+			sh = { "shellcheck" },
+			go = { "golangcilint" },
+			cpp = { "clang_check" },
+			c = { "clang_check" },
+			bash = { "bach" },
+			dart = { "flutter_analyze" },
+		}
+
+		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+	end,
 }
